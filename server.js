83aa01c5f972e6bc948eebde2b373ede8d06d938
Giordany Henriquez <giordanyhenriquez.edu@gmail.com>
2026-04-28 09:14:21 -0400

    test cambio

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("."));
+
 import dotenv from "dotenv";
 import OpenAI from "openai";
 
