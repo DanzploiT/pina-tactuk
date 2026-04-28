43755107120da53cc365772963f5999adf75e2db
Giordany Henriquez <giordanyhenriquez.edu@gmail.com>
2026-04-28 10:31:49 -0400

    ver1.1.1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./"));
 
-// 🔐 Usuarios (simulado)
+// 🔐 Usuarios
 const users = [
   { username: "admin", password: "1234" }
 ];
